9042c3db6b78ea14cfe61960c47d8ce40adcccdc
arezvani <fcairib76@gmail.com>
2023-11-17 18:24:48 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 api_url = 'https://randomuser.me/api/?results=1'
 
 # S3
-S3_ENDPOINT = 'https://abriment.com:8080'
-ACCESS_KEY_ID = '6e8a564ba6434e17acb598cccc2684a5'
-SECRET_ACCESS_KEY = '8df858ea958d484ca22f7f6d10b30c46'
-PROJECT_ID = 'e70f4c0d674748819aa2e92fd55dcc6a'
+S3_ENDPOINT = 'https://**********:****'
+ACCESS_KEY_ID = '***********'
+SECRET_ACCESS_KEY = '***********'
+PROJECT_ID = '***********'
 
 # DB
-db_host = 'dbaas.abriment.com'
-db_port = 31811
-db_username = 'admin'
-db_password = 'SAra@131064'
-db_database = 'persons'
+db_host = '***********'
+db_port = '*****'
+db_username = '*****'
+db_password = '*****'
+db_database = '*****'
